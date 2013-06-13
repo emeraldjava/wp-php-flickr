6a5b05686199910d3f8be314adcd42f01afcf56c
emeraldjava <paul.t.oconnell@gmail.com>
2013-06-13 17:25:54 +0100

    add some real code to list photo's

--- wp_flickr.php
+++ wp_flickr.php
@@ -24,8 +24,38 @@ class wp_flickr
 			), $atts ) 
 		);
 
-		//$wp_php_flickr = new wp_php_flickr('','');
-		return 'wp_flickr_list_album';//.$wp_php_flickr->people_findByUsername('bhaa');
+		$wp_php_flickr = new wp_php_flickr('','');
+		
+		$person = $wp_php_flickr->people_findByUsername('bhaa');
+		//$person = $wp_php_flickr->people_findByUsername('34896940@N06');
+		//$person = $wp_php_flickr->people_findByUsername('bhaa');//eoinfegan');//get_option('bhaa_flickr_username'));
+		// $person = $wp_php_flickr->people_findByUsername('tomhealy');//eoinfegan');//get_option('bhaa_flickr_username'));
+		
+		$photos = $wp_php_flickr->people_getPublicPhotos($person['id'], NULL, NULL, 14);
+		//var_dump($photos);
+		error_log('photos '.$photos);
+		
+		// Loop through the photos and output the html
+		foreach ((array)$photos['photos']['photo'] as $photo) {
+				
+			//error_log($photo);
+			//echo '<hr/>';
+			echo '<a href="'.$wp_php_flickr->buildPhotoURL($photo,"large").'" rel="prettyPhoto[bhaa]" >';
+			echo "<img border='0' alt='$photo[title]' src=".$wp_php_flickr->buildPhotoURL($photo, "thumbnail") . ">";
+			// http://www.flickr.com/photos/34896940@N06/8542579566/
+			//echo "<img border='0' alt='$photo[title]' src=".$wp_php_flickr->buildPhotoURL($photo, "Square") . ">";
+			echo "</a>";
+			$i++;
+			// If it reaches the sixth photo, insert a line break
+			if ($i % 6 == 0) {
+				echo "<br>\n";
+			}
+		}
+		//var_dump($list);
+		return $list;
+			
+		
+		//return 'wp_flickr_list_album';//.$wp_php_flickr->people_findByUsername('bhaa');
 	}
 }
 $wp_flickr = new wp_flickr();
