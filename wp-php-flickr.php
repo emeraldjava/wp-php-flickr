1c06525bbc475359527acd4a28aa2ddf30db0941
Paul O'Connell <paul.t.oconnell@gmail.com>
2014-09-20 11:18:48 +0100

    version

--- wp-php-flickr.php
+++ wp-php-flickr.php
@@ -10,7 +10,7 @@
  * Plugin Name:       Wordpress PHP Flickr
  * Plugin URI:        https://github.com/emeraldjava/wp-php-flickr
  * Description:       Wraps the php flickr class within a wordpress plugin
- * Version:           2014.01.25
+ * Version:           2014.09.20
  * Author:            emeraldjava
  * Author URI:        https://github.com/emeraldjava
  * Text Domain:       wp-php-flickr
